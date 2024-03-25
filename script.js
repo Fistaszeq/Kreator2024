3058eba8f14263279b033b300fa1fe9d61a266e6
Fistaszeq <87309628+Fistaszeq@users.noreply.github.com>
2024-03-25 07:33:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-70px"; // Adjust based on header height
+  }
+  prevScrollpos = currentScrollPos;
+}
